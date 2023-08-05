29542211ebeb135eb5614814212e1c6942522f32
Osman Pérez <operez@edutecno.com>
2023-08-05 11:22:21 -0400

    Registro de personas

--- server.js
+++ server.js
@@ -7,11 +7,19 @@ const app = express()
 // Recuerden que este método no admite body, deben enviar el nombre por parámetro en la URL.
 const fs = require("fs")
 
+app.use("/bootstrap", express.static(`${__dirname}/node_modules/bootstrap/dist`))
+app.use("/jquery", express.static(`${__dirname}/node_modules/jquery/dist`))
+app.use("/public", express.static(`${__dirname}/assets`))
+
 
 //Constante utilizada para conocer los tipos de archivo permitidos en la ruta upload
 const extensionesPermitidas = []
 //Constante para definir peso máximo del archivo a subir (Valor expresado en MB)
 const maximoPermitido = 3
+
+const rutaFotos = "./assets/img/fotos"
+
+const host = "http://localhost:3000"
 
 app.use(express.json())
 app.use(fileupload({ createParentPath: true}))
@@ -84,6 +92,35 @@ app.delete("/archivos/:nombre_archivo", (request, response) => {
 
 app.get("/registro", (request, response) => {
     response.sendFile(`${__dirname}/views/registro.html`)
+})
+
+app.post("/registro", async (request, response) => {
+    console.log("Body",request.body);
+    console.log("Files", request.files);
+
+    const datosFoto = request.files.foto
+    const nombreFoto = `${Date.now()}-${datosFoto.name.replace(" ","")}`
+    await datosFoto.mv(`${rutaFotos}/${nombreFoto}`)
+
+    const dataArchivo = fs.readFileSync(`${__dirname}/data/personas.txt`,"utf8")
+    const arreglo = JSON.parse(dataArchivo)
+    arreglo.push({
+        ...request.body,
+        foto: `public/img/fotos/${nombreFoto}`
+    })
+    const dataTexto = JSON.stringify(arreglo)
+    fs.writeFileSync(`${__dirname}/data/personas.txt`, dataTexto, "utf8")
+    response.json({ success: true, message: "Registro exitoso"})
+})
+
+app.get("/listado", (request, response) => {
+    const dataArchivo = fs.readFileSync(`${__dirname}/data/personas.txt`,"utf8")
+    const arreglo = JSON.parse(dataArchivo)
+    arreglo.map(item => {
+        item.foto = `${host}/${item.foto}`
+        return item
+    })
+    response.json({ success: true, message: "Listado de personas", data: arreglo})
 })
 
 
