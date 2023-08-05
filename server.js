a985886ef776942b39a5dd68a6b0fd891c89e9f2
Osman Pérez <operez@edutecno.com>
2023-08-04 20:43:08 -0400

    Listar, descargar y eliminar archivos

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const app = express()
 // Recuerden que este método no admite body, deben enviar el nombre por parámetro en la URL.
 const fs = require("fs")
 
-fs.unlinkSync
-
-
 
 //Constante utilizada para conocer los tipos de archivo permitidos en la ruta upload
 const extensionesPermitidas = []
@@ -20,6 +17,8 @@ app.use(express.json())
 app.use(fileupload({ createParentPath: true}))
 
 app.listen(3000, () => console.log("Servidor en ejecución"))
+
+const rutaBase = `${__dirname}/files`
 
 app.post("/upload", async(request, response) => {
     console.log(request.files);
@@ -42,4 +41,43 @@ app.post("/upload", async(request, response) => {
     const ruta = `${__dirname}/files/${marca}-${nombre}`
     await datosArchivo.mv(ruta)
     response.json({ success: true, message: "Ruta para subir archivo"})
+})
+
+// Ruta para listar archivos
+app.get("/archivos", async(req, res) => {
+    const archivos = fs.readdirSync(rutaBase)
+    const respuesta = []
+    for (const itemArchivo of archivos) {
+        respuesta.push({
+            nombre_archivo: itemArchivo,
+            ruta: `files/${itemArchivo}`
+        })
+    }
+    return res.json({ success: true, message: "Listado de archivos", data: respuesta})
+})
+
+
+//Ruta para descargar archivos
+app.get("/archivos/:nombre", (request, response) => {
+    const nombre = request.params.nombre
+    response.download(`${rutaBase}/${nombre}`, nombre, (err) => {
+        if(err) {
+            response.status(500).json({
+                success: false,
+                message: "Ocurrió un problema descargando el archivo"
+            })
+        }
+    })
+})
+
+
+// Ruta para eliminar archivos
+app.delete("/archivos/:nombre_archivo", (request, response) => {
+    const nombre = request.params.nombre_archivo
+    try {
+        fs.unlinkSync(`${rutaBase}/${nombre}`)
+        response.json({ success: true, message: `Archivo ${nombre} eliminado con éxito` })
+    } catch (error) {
+        response.status(500).json({ success: false, message: `Error eliminando el archivo ${nombre}`})
+    }
 })
